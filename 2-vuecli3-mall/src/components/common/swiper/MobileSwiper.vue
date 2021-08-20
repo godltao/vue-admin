44232db0a2de2675699037047cc59a798ea16218
liu.tao <liutao9126@163.com>
2021-08-20 17:30:02 +0800

    update drag

--- 2-vuecli3-mall/src/components/common/swiper/MobileSwiper.vue
+++ 2-vuecli3-mall/src/components/common/swiper/MobileSwiper.vue
@@ -1,7 +1,7 @@
 <template>
   <swiper>
     <swiper-item v-for="(item, id) in banners" :key="id">
-      <a :href="item.link">
+      <a :href="item.link" >
         <img :src="item.image" alt="">
       </a>
     </swiper-item>
